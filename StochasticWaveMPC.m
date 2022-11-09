8080f2399520f5e983f49b403e8d5226b301b6c4
Matthias K. Hoffmann <matthias.hoffmann@uni-saarland.de>
2022-11-09 10:00:49 +0100

    Minor changes, font size in figures halved

--- StochasticWaveMPC.m
+++ StochasticWaveMPC.m
@@ -1,5 +1,5 @@
 %% Runs MPC algorithm for wave harvester
-
+% This script requires the installation of the CasADi toolbox, available at https://web.casadi.org/
 
 % Define MPC parameters. These can and should be chosen by the user.
 % Suggested
@@ -10,9 +10,9 @@ timehorizon     = 10;          % [1-inf]  How long
 timestep        = 0.5;         % [0.05-1] MPC timestep, i.e. the discretisation of the ocp.
                                %          A step larger then 1 is not recommended.
 Seed            = 2  ;         % [1-10]   Seed of the Wave distrurbance. Seeds [1-10] have been provided.
-Damagereduction = 0.4;         % [0-1]    This implementation uses Multicriterial Optimization.
-                               %          Therefore we need to specify a weight forthe damage contribution. 
-                               %          0 wouldcorrespond to a single criterial  optimization only
+Damagereduction = 0.4;         % [0-1]    This implementation uses multi-criteria Optimization.
+                               %          Therefore we need to specify a weight for the damage contribution. 
+                               %          0 would correspond to single-objective optimization only
                                %          maximizing the harvested energy
                                       
 
@@ -32,6 +32,7 @@ foh             = 1;                                % First order hold reconstru
 
 weight = [1-Damagereduction Damagereduction];       % Weight for Weighted Sum MOOP
 uMax            = 33^2;                             % input voltage is limited
+
 % Construct the OCP
 % The simulation is started with the flap already in motion. The initial
 % state x0 represents the state after 100 seconds of swing in.
@@ -42,24 +43,25 @@ uMax            = 33^2;                             % input voltage is limited
 % x(7)   = Damage aculmulated
 x0 =   [0.0515     0.1392   314.4363   94.1062  190.4844         0         0 ]';
 
-% import casadi and set solver
-import casadi.*
+% set solver
 ocp = casadi.Opti();
 ocp.solver('ipopt')
 
-% The wave differential equation
+% The differential equation describing the DEG-WEC plus the integrands of
+% the cost functions.
 wave_dgl = @(x,u,d) [Ac * x(1:5) - Bc * 1e6 * u * gamma * x(2) + Bc * d;
     cost_energy(x,u,d);
     cost_damage(x,u);
     ];
-% create the decision variables for the ocp.
+
+% create the decision variables and parameters for the ocp.
 x0_p = ocp.parameter(nx,1);
 x = [x0_p, ocp.variable(nx, nSteps+1 - (foh))];
 u = [ocp.parameter(nu, 1), ocp.variable(nu, nSteps)];
 d = ocp.parameter(1, nSteps + foh);
 h_integ = casadi.MX(nx, nSteps);
 
-% integrate the discretised ode. x(t+1)  = integrator(x(t),u(t),d(t))
+% integrate the discretised ode. x(t+1) = integrator(x(t),u(t),d(t))
 for iStep = 1:nSteps
     h_integ(:, iStep) = integrator_step_disturbed(x(:,iStep), u(:, iStep + (0:foh)), timestep, wave_dgl, d(:, iStep + (0:foh))) - x(:,iStep+1);
     ocp.subject_to( )
@@ -72,16 +74,16 @@ ocp.subject_to( h_zero )
 try
     Wave_function = @(t) interp1(WaveData.time,WaveData.(['Wave_Seed_' num2str(Seed)]),t);
 catch
-    error('Please choose Seed between 1 and 10!')
+    error('Please choose Seed between 1 and 10 or provide your own wave data!')
 end
-ocp.set_value(d,arrayfun(@(t)Wave_function(t),time));
-ocp.set_value(x0_p,x0)
+ocp.set_value(d, arrayfun(@(t)Wave_function(t), time));
+ocp.set_value(x0_p,  x0)
 ocp.set_value(u(1),0)
 costfun = ([x(6,end), x(7,end)]);
 p_params = ocp.parameter(2,1);
 ocp.minimize( costfun*p_params );
-ocp.set_value(p_params,weight);
-ocp.subject_to(0<=u<=uMax);
+ocp.set_value(p_params, weight);
+ocp.subject_to(0 <= u <= uMax);
 
 % the MPC starts here. The MPC solutions are tracked in the
 % ResultsMPC array. The applied signal is stored in the AppliedSignal array
@@ -89,7 +91,7 @@ ocp.subject_to(0<=u<=uMax);
 % AppliedSignal (2,:) : Applied Voltage
 % AppliedSignal (3,:) : Angle of Flap
 
-starttime = [0:timestep:timehorizon];
+starttime = 0:timestep:timehorizon;
 ResultsMPC = cell(length(starttime),1);
 AppliedSignal=[];
 if (plotting)
@@ -128,8 +130,8 @@ for i = 1:length(starttime)
     try
         solOld = ocp.solve();
     catch
-        % Faster solver sometimes cannot find solution.If that is the case
-        % disable warmstart for one iteration
+        % The chosen warmstart parameters do not always work. If that is the case
+        % disable warmstart for one iteration step.
         ocp.solver('ipopt')
         solOld = ocp.solve();
         solver_opt = struct('warm_start_init_point', 'yes', 'mu_init', 1e-6, 'warm_start_mult_bound_push',1e-8, ...
@@ -149,26 +151,31 @@ for i = 1:length(starttime)
 
 
     if (plotting)
-        axes(ax1)
+        axes(ax1);
         hold on
         plot(AppliedSignal(1,:),rad2deg(AppliedSignal(3,:)),'b',LineWidth=8)
         plot(solOld.value(time),rad2deg(solOld.value(x(2,:))),'r--',LineWidth=8)
+        
         if ~(i==1)
             delete(ax1.Children(end))
             delete(ax1.Children(end))
         end
-        axes(ax2)
+
+        axes(ax2);
         hold on
         plot(AppliedSignal(1,:),AppliedSignal(2,:),'b',LineWidth=8)
         plot(solOld.value(time),solOld.value(u),'r--',LineWidth=8)
+        
         if ~(i==1)
             delete(ax2.Children(end))
             delete(ax2.Children(end))
         end
-        axes(ax3)
+        
+        axes(ax3);
         hold on
         plot(AppliedSignal(1,:),arrayfun(@(t) Wave_function(t),AppliedSignal(1,:)),'b',LineWidth=8)
         plot(solOld.value(time),arrayfun(@(t) Wave_function(t),solOld.value(time)),'r--',LineWidth=8)
+        
         if ~(i==1)
             delete(ax3.Children(end))
             delete(ax3.Children(end))
@@ -183,13 +190,13 @@ if (saving)
     save(['Results/MPC_Horizon_eq_' num2str(timehorizon) '_DamageReduction_eq_ ' num2str(Damagereduction) '_Seed_eq_' num2str(Seed) '.mat'  ],"ResultsMPC",'time')
 end
 
-
+%% Damage cost function
 function cost = cost_damage(x, u, ~, ~)
 %     cost = (max(u - 484/(cos(x(2)).^2), 0).^2)*1e-6;
 cost = (max(u - 484, 0).^2)*1e-6;
 end
 
-
+%% Energy cost function
 function cost = cost_energy(x, u, d)
 persistent Ch S R0
 
@@ -200,10 +207,9 @@ cost = (Ch*x(1).^2 + x(3:5)'*S*x(3:5) - d .* x(1))*1e-6 + u/R0;
 end
 
 
-% runge-kutta 4  integrator
+%% runge-kutta 4  integrator
 function x_end = integrator_step_disturbed(x0, u, dt, odefun, d)
 % calculate one integration step with step size dt
-import casadi.*
 
 x0_rk = x0;
 k = casadi.MX( size( x0, 1 ), 4 );
